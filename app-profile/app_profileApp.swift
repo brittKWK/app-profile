7864079443baefc43fd0432f0cfc7c7980659763
Britney <queenbrit101@gmail.com>
2023-08-03 13:20:48 -0400

    Initial Commit

---
+++ app-profile/app_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_profileApp.swift
+//  app-profile
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct app_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
